6914f98feb33a764b99b5eae7eb94ab014ca26d5
Bhargav Kumar Diwakar <bhargavkumardiwakar@gmail.com>
2026-04-25 00:09:38 +0530

    feat: add Party entity

---
+++ nyayalay/src/main/java/com/nyayalay/entity/Party.java
@@ -0,0 +1,24 @@
+package com.nyayalay.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="parties")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Party {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String role;
+    private String contactNumber;
+    private String email;
+    private String address;
+}
