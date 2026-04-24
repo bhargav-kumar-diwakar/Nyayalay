14e42da8836d18e2f2b48c66ce2686a262ed2ab4
Bhargav Kumar Diwakar <bhargavkumardiwakar@gmail.com>
2026-04-25 00:12:53 +0530

    feat: add Document entity

---
+++ nyayalay/src/main/java/com/nyayalay/entity/Document.java
@@ -0,0 +1,25 @@
+package com.nyayalay.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "documents")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileName;
+    private String fileType;
+    private String fileUrl;
+    private LocalDate uploadDate;
+
+}
